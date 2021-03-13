a6b0df98b21c05f242563055e7998bc0f40d31d9
Vinaya Kumar <vinay9415756155@gmail.com>
2021-03-14 01:28:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const compression = require("compression");
+
+const app = express();
+app.use(compression());
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server started at 3000");
+});
